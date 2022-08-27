6667ff7cf8126c9891a6632a287e1b53b6680e5e
naresh-webdev <gkmnaresh01@gmail.com>
2022-08-27 23:45:49 +0530

    added google map

--- script.js
+++ script.js
@@ -24,6 +24,14 @@ const cursor = document.querySelector(".cursor");
 //   }
 // });
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+    mapId: "48a55d1d737e8099",
+  });
+}
+
 //?section-1.preload
 
 window.addEventListener("load", function () {
