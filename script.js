7f1a7f8b5a719675ada56031e15b8a37c64e6de6
naresh-webdev <gkmnaresh01@gmail.com>
2022-09-08 19:34:09 +0530

    section 2 almost complete

--- script.js
+++ script.js
@@ -43,10 +43,33 @@ window.addEventListener("load", function () {
     document.querySelector(".header__preload").classList.add("hidden");
     section1.classList.remove("hidden");
     section2.classList.remove("hidden");
+    document.querySelectorAll(".down").forEach((arr) => {
+      arr.classList.remove("hidden");
+    });
     // section3.classList.remove("hidden");
     // section4.classList.remove("hidden");
     // section5.classList.remove("hidden");
     // error.classList.remove("hidden");
+
+    const observer = new IntersectionObserver(
+      (e) => {
+        console.log(e[0].isIntersecting);
+        if (e[0].isIntersecting) {
+          document.querySelectorAll(".down").forEach((arr) => {
+            arr.classList.add("hidden");
+          });
+        } else {
+          document.querySelectorAll(".down").forEach((arr) => {
+            arr.classList.remove("hidden");
+          });
+        }
+      },
+      {
+        root: null,
+        threshold: 0,
+      }
+    );
+    observer.observe(section2);
   }, 2500);
 });
 
